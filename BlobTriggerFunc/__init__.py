7dccf9a813217d393aece85198df6eef24a7f14c
Evan Dai <evandai@amrock.com>
2021-10-24 13:11:57 -0400

    nothing

--- BlobTriggerFunc/__init__.py
+++ BlobTriggerFunc/__init__.py
@@ -1,7 +1,9 @@
 import logging
-
+import os
 import azure.functions as func
-
+from azureml.core.authentication import ServicePrincipalAuthentication
+from azureml.core import Workspace
+from azureml.pipeline.core.pipeline_endpoint import PipelineEndpoint
 
 def main(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
