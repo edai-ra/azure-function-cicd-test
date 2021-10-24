a12a061abf816b4805edd5b0d76202c4ee072c79
Evan Dai <evandai@amrock.com>
2021-10-24 14:27:05 -0400

    add system path

--- BlobTriggerFunc/__init__.py
+++ BlobTriggerFunc/__init__.py
@@ -4,7 +4,8 @@ import azure.functions as func
 from azureml.core.authentication import ServicePrincipalAuthentication
 from azureml.core import Workspace
 from azureml.pipeline.core.pipeline_endpoint import PipelineEndpoint
-
+import sys
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 def main(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
